e4b1a7c19e724050de83dbf6237b9cb5614896d7
Mike Riess <mikeriess@gmail.com>
2014-06-16 22:06:05 -0700

    [Chapter 4] - Linked Lists
    Experiment implementing linked lists in
    Ruby, and the relative performance difference
    to using the stdlib Array.

---
+++ linked_list.rb
@@ -0,0 +1,108 @@
+require 'benchmark'
+
+Node = Struct.new(:value, :next_node)
+
+def new_node(linked_list, value)
+	new_node = Node.new(value, linked_list.next_node)
+	linked_list.next_node = new_node
+end
+
+Benchmark.bmbm do |bm|
+	bm.report('array - build') do
+		1000.times do
+			a = []
+
+			1000.times do
+				a.unshift(1)
+			end
+		end
+	end
+
+	bm.report('linked list - build') do
+		1000.times do
+			linked_list = Node.new(1)
+
+			1000.times do
+				new_node(linked_list, 1)
+			end
+		end
+	end
+
+	bm.report('array - length') do
+		1000.times do
+			standard_length_a = Array.new(1000, 1)
+			standard_length_a.length
+		end
+	end
+
+	bm.report('linked list - length') do
+		1000.times do
+			linked_length_l = Node.new(1)
+			999.times do
+				new_node(linked_length_l, 1)
+			end
+
+			length = 0
+			linked_list = linked_length_l
+
+			while linked_list.next_node != nil
+				length += 1
+				linked_list = linked_list.next_node
+			end
+
+			length
+		end
+	end
+
+	bm.report('array - average') do
+		1000.times do
+			standard_average_a = Array.new(1000, 1)
+			length = 0
+			sum = 0
+			standard_average_a.each do |n|
+				length += 1
+				sum += n
+			end
+
+			sum / length
+		end
+	end
+
+	bm.report('linked list - average') do
+		1000.times do
+			linked_average_l = Node.new(1)
+			999.times do
+				new_node(linked_average_l, 1)
+			end
+			length = 0
+			sum = 0
+			linked_list = linked_average_l
+
+			while linked_list.next_node != nil
+				length += 1
+				sum += linked_list.next_node.value
+				linked_list = linked_list.next_node
+			end
+
+			sum / length
+		end
+	end
+end
+
+# 2.0.0-p0 :001 > load 'think_like_a_programmer/linked_list.rb'
+# Rehearsal -----------------------------------------------------------
+# array - build      0.200000   0.000000   0.200000 (  0.202653)
+# linked list - build       0.820000   0.000000   0.820000 (  0.822774)
+# array - length   0.000000   0.010000   0.010000 (  0.007899)
+# linked list - length    1.110000   0.000000   1.110000 (  1.111479)
+# array - average    0.140000   0.010000   0.150000 (  0.146595)
+# linked list - average     1.240000   0.000000   1.240000 (  1.252504)
+# -------------------------------------------------- total: 3.530000sec
+
+#                               user     system      total        real
+# array - build      0.190000   0.000000   0.190000 (  0.195571)
+# linked list - build       0.800000   0.000000   0.800000 (  0.809380)
+# array - length   0.000000   0.000000   0.000000 (  0.008035)
+# linked list - length    1.110000   0.000000   1.110000 (  1.141347)
+# array - average    0.150000   0.000000   0.150000 (  0.150367)
+# linked list - average     1.240000   0.010000   1.250000 (  1.256540)
